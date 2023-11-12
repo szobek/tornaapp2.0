a3cf7b8861bad6b97a12a23ffa3e4a043e50b477
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-11-12 17:06:46 +0100

    Login function

--- src/main/java/LoginForm.java
+++ src/main/java/LoginForm.java
@@ -6,20 +6,37 @@ public class LoginForm extends JFrame {
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JPanel loginPanel;
-    private JButton button1;
+    private JButton btnLogin;
 
 
     public LoginForm() {
         setContentPane(loginPanel);
         setTitle("login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+textField1.setText("kunszt.norbert@gmail.com");
+passwordField1.setText("rrrrrr");
         pack();
         setVisible(true);
-        button1.addActionListener(new ActionListener() {
+
+        btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-          openAnother();
+
+                String email = textField1.getText();
+                String psw = PasswordHash.hashing(String.valueOf(passwordField1.getPassword()));
+
+                if(email.isEmpty())JOptionPane.showMessageDialog(null,"Email nem lehet üres");
+
+                if(psw.isEmpty())JOptionPane.showMessageDialog(null,"Jelszó nem lehet üres");
+
+                User user = DBHandler.checkLogin(email,psw);
+
+                if(user!=null){
+                    openAnother();
+                } else {
+                    JOptionPane.showMessageDialog(null,"Hibás adatok");
+                }
+                System.out.println(user);
             }
         });
     }
