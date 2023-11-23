535a409de7266b267defba865da3ce7960a44fff
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-11-23 10:45:02 +0100

    fix warning in modal

--- src/main/java/SetUserRighsModal.java
+++ src/main/java/SetUserRighsModal.java
@@ -43,7 +43,7 @@ public class SetUserRighsModal {
         });
         btnCancel.addActionListener(e -> dialog.dispose());
         setInputData();
-
+buttonPanel.setSize(400,200);
 
         dialog.setVisible(true);
         dialog.pack();
